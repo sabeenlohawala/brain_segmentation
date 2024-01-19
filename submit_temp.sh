73766fa3343ab1f9bd2af1a5f4c201a618bd469a
Sabeen Loahwala <sabeen@mit.edu>
2024-01-19 11:21:37 -0500

    Update submit_temp.sh

--- submit_temp.sh
+++ submit_temp.sh
@@ -6,8 +6,8 @@
 #SBATCH --gres=gpu:a100:4
 #SBATCH --mem=40G # per node memory
 #SBATCH -p gablab
-#SBATCH -o ./slurm_outputs/test-small-50seg.out
-#SBATCH -e ./slurm_outputs/test-small-50seg.err
+#SBATCH -o ./logs/test-small-50seg-simpleunet.out
+#SBATCH -e ./logs/test-small-50seg-simpleunet.err
 #SBATCH --mail-user=sabeen@mit.edu
 #SBATCH --mail-type=FAIL
 
@@ -17,7 +17,8 @@ conda init bash
 # -u ensures that the output is unbuffered, and written immediately to stdout.
 # 24 batch size per A100 GPU
 # For multi GPU training
-srun python -u scripts/commands/main.py train --logdir='20231211-test-2gpu-Msegformer\Ldice\C51\B512\A0' --num_epochs=20 --batch_size=512 --model_name='segformer' --nr_of_classes=51 --lr=5e-5
+srun python -u scripts/commands/main.py train --logdir='20240119-test-single-4gpu-Msegformer\Ldice\C51\B512\A0' --num_epochs=2 --batch_size=512 --model_name='segformer' --nr_of_classes=51 --lr=5e-5
+# srun python -u scripts/commands/main.py train --logdir='20240118-4gpu-Msimple_unet\Ldice\C51\B370\A0' --num_epochs=1000 --batch_size=370 --model_name='simple_unet' --nr_of_classes=51 --lr=5e-5
 
 # to run:
 # sbatch --export=ALL,wandb_description='testrun' jobs/job.sh
