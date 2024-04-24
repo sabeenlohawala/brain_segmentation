bc190049ed5a721b2ed843a4f4afc9e83d4b5ce1
Sabeen Loahwala <sabeen@mit.edu>
2024-04-24 18:18:22 -0400

    Add train-val-test split to new optimized dataset

--- scripts/mit_kwyk_data_optimized.py
+++ scripts/mit_kwyk_data_optimized.py
@@ -6,6 +6,7 @@ from multiprocessing import Pool
 import nibabel as nib
 import numpy as np
 import torch
+from sklearn.model_selection import train_test_split
 
 DATA_DIR = "/nese/mit/group/sig/data/kwyk/rawdata"
 N_VOLS = 10  # number of volumes to load (this is only for testing)
@@ -28,39 +29,28 @@ def main_timer(func):
     return function_wrapper
 
 
-def calculate_bg(feature_file):
-    print(os.path.basename(feature_file))
-    feature_vol = (
-        nib.load(feature_file).get_fdata().astype(np.uint)
-    )  # btw shouldn't this be done on label maps?
+def calculate_bg(label_file):
+    print(os.path.basename(label_file))
+    label_vol = (nib.load(label_file).get_fdata().astype(np.int16))
 
-    bgcount_ax1 = [
-        np.count_nonzero(feature_vol[i, :, :] == 0) for i in range(256)
-    ]  # TODO: bg percentages
-    bgcount_ax2 = [
-        np.count_nonzero(feature_vol[:, i, :] == 0) for i in range(256)
-    ]  # TODO: optimizing this if you can and want
-    bgcount_ax3 = [np.count_nonzero(feature_vol[:, :, i] == 0) for i in range(256)]
+    bgcount_ax0 = np.sum(label_vol == 0, axis=(1,2)) / 256**2
+    bgcount_ax1 = np.sum(label_vol == 0, axis=(0,2)) / 256**2
+    bgcount_ax2 = np.sum(label_vol == 0, axis=(0,1)) / 256**2
 
-    # is the above the same as (Please check for me): if they match then for loop is not needed
-    # bgcount_ax1 = np.sum(np.count_nonzero(feature_vol == 0, axis=1), axis=2)  # for axis 0
-    # bgcount_ax2 = np.sum(np.count_nonzero(feature_vol == 0, axis=0), axis=2)  # for axis 1
-    # bgcount_ax3 = np.sum(np.count_nonzero(feature_vol == 0, axis=0), axis=1)  # for axis 2
-
-    bg_count = np.stack((bgcount_ax1, bgcount_ax2, bgcount_ax3), axis=0)
+    bg_count = np.stack((bgcount_ax0, bgcount_ax1, bgcount_ax2), axis=0)
     return bg_count
 
 
 @main_timer
 def main():
-    feature_files = sorted(glob.glob(os.path.join(DATA_DIR, "*orig*.nii.gz")))[:N_VOLS]
+    # feature_files = sorted(glob.glob(os.path.join(DATA_DIR, "*orig*.nii.gz")))[:N_VOLS]
     label_files = sorted(glob.glob(os.path.join(DATA_DIR, "*aseg*.nii.gz")))[:N_VOLS]
 
     nprocs = len(os.sched_getaffinity(0))
     with Pool(processes=nprocs) as p:
         output = p.map(
-            calculate_bg, feature_files
-        )  # passing in labels is redundant... you know how to change this
+            calculate_bg, label_files
+        ) 
 
     final_output = np.dstack(output)  # [num_slices, num_directions, num_files]
     final_output = np.moveaxis(
@@ -70,49 +60,50 @@ def main():
 
 
 class SampleDataset(torch.utils.data.Dataset):
-    def __init__(self, bg_count=30000):  # change this to percent
+    def __init__(self, mode, bg_percent=0.99):
         self.matrix = np.load("output.npy", allow_pickle=True)
         self.matrix = torch.Tensor(self.matrix)
 
-        self.filtered_matrix = self.matrix > bg_count
-        self.feature_vols = sorted(glob.glob(os.path.join(DATA_DIR, "*orig*.nii.gz")))[
-            :N_VOLS
-        ]
-        self.label_vols = sorted(glob.glob(os.path.join(DATA_DIR, "*aseg*.nii.gz")))[
-            :N_VOLS
-        ]
-        # the above 2 lines can be a tuple if you prefer
+        self.feature_label_files = list(zip(
+            sorted(glob.glob(os.path.join(DATA_DIR, "*orig*.nii.gz")))[:self.matrix.shape[0]],
+            sorted(glob.glob(os.path.join(DATA_DIR, "*aseg*.nii.gz")))[:self.matrix.shape[0]]
+        ))
 
-        self.nz_indices = torch.nonzero(
+        # perform train-val-test split and apply to self.matrix and self.feature_label_files
+        indices = np.arange(self.matrix.shape[0])
+        train_indices, remaining_indices = train_test_split(indices, test_size=0.2, random_state=42)
+        validation_indices, test_indices = train_test_split(remaining_indices, test_size=0.5, random_state=42)
+        if mode == 'train':
+            keep_indices = train_indices
+        elif mode == 'val' or mode == 'validation':
+            keep_indices = validation_indices
+        elif mode == 'test':
+            keep_indices = test_indices
+        else:
+            raise Exception(
+                f"{mode} is not a valid data mode. Choose from 'train', 'test', or 'validation'."
+            )
+        self.matrix = self.matrix[keep_indices]
+        self.feature_label_files = [feature_label_pair for i, feature_label_pair in enumerate(self.feature_label_files) if i in keep_indices]
+
+        self.filtered_matrix = self.matrix < bg_percent
+        self.nonzero_indices = torch.nonzero(
             self.filtered_matrix
         )  # [num_slices, 3] - (file_idx, direction_idx, slice_idx)
 
-        assert self.nz_indices.shape[0] <= torch.numel(
+        assert self.nonzero_indices.shape[0] <= torch.numel(
             self.matrix
         ), "select bg slice count cannot be more than total slice count"
 
     def __getitem__(self, index):
-        file_idx, direction_idx, slice_idx = self.nz_indices[index]
+        file_idx, direction_idx, slice_idx = self.nonzero_indices[index]
 
-        feature_vol = self.feature_vols[file_idx]
-        label_vol = self.label_vols[file_idx]
+        feature_file, label_file = self.feature_label_files[file_idx]
 
-        feature_vol = torch.from_numpy(nib.load(feature_vol).get_fdata())
-        label_vol = torch.from_numpy(nib.load(label_vol).get_fdata())
+        feature_vol = torch.from_numpy(nib.load(feature_file).get_fdata().astype(np.float32))
+        label_vol = torch.from_numpy(nib.load(label_file).get_fdata().astype(np.int16))
 
         # do you want a cropped slice (sure do it here, of course you still need that fixed number)
-
-        # if direction_idx == 0:
-        #     feature_slice = feature_vol[slice_idx, :, :]
-        #     label_slice = label_vol[slice_idx, :, :]
-
-        # if direction_idx == 1:
-        #     feature_slice = feature_vol[:, slice_idx, :]
-        #     label_slice = label_vol[:, slice_idx, :]
-
-        # if direction_idx == 2:
-        #     feature_slice = feature_vol[:, :, slice_idx]
-        #     label_slice = label_vol[:, :, slice_idx]
 
         # all the above 3 if conditions can be written in a single line:
         feature_slice = torch.index_select(
@@ -123,18 +114,17 @@ class SampleDataset(torch.utils.data.Dataset):
         )
 
         # TODO:
-        # where and how to do the train/val/test split: on the npy matrix that you loaded. agree?
         # i now wonder if rotating as well can be dont at get time...probably not advisable unless you have torch functions to do the same
         # (if you care) see nitorch (written by yael) or neurite (written by adrian)
 
         return (feature_slice, label_slice)
 
     def __len__(self):
-        return self.nz_indices.shape[0]
+        return self.nonzero_indices.shape[0]
 
 
 if __name__ == "__main__":
-    main()
-    # dataset = SampleDataset()
-    # a, b = dataset[0]
-    # print(a.shape, b.shape)
+    # main()
+    dataset = SampleDataset(mode='validation',bg_percent=0.8)
+    a, b = dataset[0]
+    print(a.shape, b.shape)
