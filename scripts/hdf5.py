bf0245b1e54891f1ff8950f814e9b68b23c4fe5f
Sabeen Loahwala <sabeen@mit.edu>
2024-04-26 13:03:51 -0400

    Add assertion to check scale factors

--- scripts/hdf5.py
+++ scripts/hdf5.py
@@ -6,17 +6,15 @@ import numpy as np
 from TissueLabeling.utils import main_timer
 from multiprocessing import Pool
 
+SAVE_NAME = "/om2/user/sabeen/kwyk_data/satra.h5"
 @main_timer
 def write_kwyk_hdf5():
     N_VOLS = 10
-    feature_files = sorted(
-        glob.glob("/om2/scratch/Mon/sabeen/kwyk-volumes/rawdata/*orig*")
-    )[:N_VOLS]
-    label_files = sorted(
-        glob.glob("/om2/scratch/Mon/sabeen/kwyk-volumes/rawdata/*aseg*")
-    )[:N_VOLS]
+    feature_files = sorted(glob.glob("/om2/scratch/Mon/sabeen/kwyk-volumes/rawdata/*orig*"))[:N_VOLS]
+    label_files = sorted(glob.glob("/om2/scratch/Mon/sabeen/kwyk-volumes/rawdata/*aseg*"))[:N_VOLS]
+    feature_label_files = zip(feature_files, label_files)
 
-    f = h5.File("kwyk.h5", "w")
+    f = h5.File(SAVE_NAME, "w")
     features = f.create_dataset(
         "kwyk_features",
         (N_VOLS, 256, 256, 256),
@@ -42,14 +40,21 @@ def write_kwyk_hdf5():
     # with Pool(processes=len(os.sched_getaffinity(0))) as pool:
     #     pool.map(write_volume, zip(feature_files, label_files))
 
-    for idx, (feature_file, label_file) in enumerate(zip(feature_files, label_files)):
+    # check scale factors are all nan
+    nib_files = [nib.load(file) for file in feature_files]
+    scl_slopes = np.array([file.header['scl_slope'] for file in nib_files])
+    scl_inters = np.array([file.header['scl_inter'] for file in nib_files])
+    assert np.isnan(scl_slopes).all() and np.isnan(scl_inters).all()
+    print('Assertion passed!')
+
+    for idx, (feature_file, label_file) in enumerate(feature_label_files):
         features[idx, :, :, :] = nib.load(feature_file).dataobj
         labels[idx, :, :, :] = nib.load(label_file).dataobj
 
 
 @main_timer
 def read_kwyk_hdf5():
-    kwyk = h5.File("kwyk.h5", "r")
+    kwyk = h5.File(SAVE_NAME, "r")
     features = kwyk["kwyk_features"]
     labels = kwyk["kwyk_labels"]
 
